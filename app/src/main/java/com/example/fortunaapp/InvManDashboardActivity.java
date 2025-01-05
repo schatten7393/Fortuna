dc57f8ec9e09a11d04fbd0076ffbefa7515f762c
FeRN <fasakhor@gmail.com>
2025-01-05 10:30:07 +0600

    adding necessary drawable resources and some activities

---
+++ app/src/main/java/com/example/fortunaapp/InvManDashboardActivity.java
@@ -0,0 +1,23 @@
+package com.example.fortunaapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class InvManDashboardActivity extends AppCompatActivity {
+
+    Button menuButton= findViewById(R.id.menuButton);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inv_man_dashboard);
+
+    }
+}
