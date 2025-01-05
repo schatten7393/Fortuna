cb8e2bcbae68dc83266493fb98df291d2343007b
FeRN <fasakhor@gmail.com>
2025-01-05 22:05:57 +0600

    adding more necessary drawable resources, colors, strings and some activities

--- app/src/main/java/com/example/fortunaapp/InventoryActivity.java
+++ app/src/main/java/com/example/fortunaapp/InventoryActivity.java
@@ -1,7 +1,6 @@
 package com.example.fortunaapp;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InvManDashboardActivity extends AppCompatActivity {
-
-    Button logoutButton, homeButton, profileButton;
+public class InventoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inv_man_dashboard);
+        setContentView(R.layout.activity_inventory);
 
     }
 }
